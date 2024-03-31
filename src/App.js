d3c3ffd151c56758a2e22ea67bd3b6a5db60eeb4
daminisherathiya <daminisherathiya@gmail.com>
2024-04-01 00:37:55 +0530

    Install Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Button from '@mui/material/Button';
 function App() {
   return (
     <div className="tw-text-center tw-text-4xl">
       Otlub
+      <Button variant="contained">Hello world</Button>
     </div>
   );
 }
